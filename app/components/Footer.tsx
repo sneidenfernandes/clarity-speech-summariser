b75e9e82b00a9c27b3bf094b9dbc085b0cb5a8cf
sneidenfernandes <sneiden@gmail.com>
2025-07-02 20:38:47 +0100

    footer changed

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='w-full fixed bottom-0 h-7 text-xs bg-zinc-800 text-zinc-200/70 flex justify-center items-center font-light'>
+    <div className='w-full fixed bottom-0 h-7 text-xs bg-zinc-700 text-zinc-200/70 flex justify-center items-center font-light'>
       Built by 
       &nbsp; 
       <a href="https://sneiden.com" className='font-semibold hover:underline'>Sneiden</a>
