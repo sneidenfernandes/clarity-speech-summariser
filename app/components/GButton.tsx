db4bfd859fd821fdde9a492bde829c38bf3d452e
sneidenfernandes <sneiden@gmail.com>
2025-07-08 23:25:43 +0100

    more gradients

--- app/components/GButton.tsx
+++ app/components/GButton.tsx
@@ -10,9 +10,8 @@ export default function GButton(){
         const response = await signIn("google", {callbackUrl: "/home"});
     }
         
-
     return <div className="flex items-center justify-center bg-gradient-to-r from-neutral-950 via-neutral-900 to-neutral-950">
-            <button onClick={handeSignin} className="px-4 py-2 bg-neutral-900 border flex gap-2 border-neutral-800  rounded-lg text-neutral-400 hover:border-zinc-700 hover:text-zinc-200 hover:shadow transition duration-150">
+            <button onClick={handeSignin} className="px-4 py-2 bg-neutral-900 border flex gap-2 border-zinc-600  rounded-lg text-neutral-400 hover:border-zinc-600 hover:text-zinc-200 hover:shadow hover:shadow-white/20 transition-all duration-150 ease-out">
             <img className="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo"></img>
              <span>Login with Google</span>
             </button>
