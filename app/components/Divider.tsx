ad30769a8453eb55af16f180f06fd6d89078cb40
sneidenfernandes <sneiden@gmail.com>
2025-07-09 02:52:29 +0100

    divider brigher on landing

--- app/components/Divider.tsx
+++ app/components/Divider.tsx
@@ -3,8 +3,8 @@
 export default function Divider(){
 
     return <div className="inline-flex items-center justify-center w-full py-2">
-    <hr className="w-30 h-[1px] my-8 border-0 bg-white opacity-20"></hr>
-    <span className="text-white opacity-40 text-xs px-2">or</span>
-    <hr className="w-30 h-[1px] my-8 border-0 bg-white opacity-20"></hr>
+    <hr className="w-30 h-[1px] my-8 border-0 bg-white opacity-30"></hr>
+    <span className="text-white opacity-50 text-xs px-2">or</span>
+    <hr className="w-30 h-[1px] my-8 border-0 bg-white opacity-30"></hr>
  </div>
 }
